4a40be8b6dd6754be0d1e352c6ffd42ee978a1ad
DeepEsh <54444594+deep-esh7@users.noreply.github.com>
2020-12-11 16:32:53 +0530

    Update appConfig.js

--- server/config/appConfig.js
+++ server/config/appConfig.js
@@ -1,6 +1,6 @@
 const appConfig = {
   port: process.env.PORT || 3000,
-  hostUrl: process.env.HOST_URL || `http://localhost`
+  hostUrl: process.env.HOST_URL || `http://paytmchecksum7.herokuapp.com/`
 };
 
 const logConfig = {
@@ -8,8 +8,8 @@ const logConfig = {
 };
 
 var paytmConfig = {
-	mid: process.env.PAYTM_MERCH_ID || '<YOUR-MERCH-ID>',
-	key: process.env.PAYTM_MERCH_KEY || '<YOUR-MERCH-KEY>',
+	mid: process.env.PAYTM_MERCH_ID || 'qCGeng20200603598739',
+	key: process.env.PAYTM_MERCH_KEY || 'W_ipsL9Yhqq8!x@z',
   website: process.env.PAYTM_MERCH_WEBSITE || 'WEBSTAGING',
   transactionUrl: process.env.PAYTM_TXN_URL || 'https://securegw-stage.paytm.in/theia/processTransaction',
   hostName: process.env.PAYTM_HOST || 'securegw-stage.paytm.in'
