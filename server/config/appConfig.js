e43c460b76f078e61c8aec6ed93cac47b3d98ae6
DeepEsh <54444594+deep-esh7@users.noreply.github.com>
2020-12-11 20:54:40 +0530

    Update appConfig.js

--- server/config/appConfig.js
+++ server/config/appConfig.js
@@ -8,8 +8,10 @@ const logConfig = {
 };
 
 var paytmConfig = {
-	mid: process.env.PAYTM_MERCH_ID || 'UpNKDA70368281575842',
-	key: process.env.PAYTM_MERCH_KEY || 'CONALvaAM4vMejWB',
+	mid: process.env.PAYTM_MERCH_ID || 'qCGeng20200603598739',
+//   qCGeng20200603598739
+	key: process.env.PAYTM_MERCH_KEY || 'W_ipsL9Yhqq8!x@z',
+//   CONALvaAM4vMejWB
   website: process.env.PAYTM_MERCH_WEBSITE || 'WEBSTAGING',
   transactionUrl: process.env.PAYTM_TXN_URL || 'https://securegw.paytm.in/order/process',
   hostName: process.env.PAYTM_HOST || 'securegw-stage.paytm.in'
